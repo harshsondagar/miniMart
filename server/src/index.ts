83f2ba86916a58a9cfbcb9ec3080b36c1af1202a
harsh <harshsondagar@weetechsolution.com>
2026-07-07 11:51:14 +0530

    Fix: converted server folder to regular directory

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+import v1Router from "./routes/v1"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", v1Router)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`app is running port : ${PORT}`);
+})
