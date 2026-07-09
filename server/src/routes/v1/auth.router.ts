245e0b3c51fcb0c3f13fec86e8d7796774c2c923
harsh <harshsondagar@weetechsolution.com>
2026-07-09 11:27:28 +0530

    feat/auth: add jwt auth

--- server/src/routes/v1/auth.router.ts
+++ server/src/routes/v1/auth.router.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { authController } from "../../controllers/auth.controller";
 
 const authRouter = Router()
 
-authRouter.post("register")
+authRouter.post("/register", authController.register)
+authRouter.post("/login", authController.login)
+authRouter.post("/logout", authController.logout)
 
 export default authRouter
+
--- server/src/services/auth.services.ts
+++ server/src/services/auth.services.ts
@@ -1,5 +1,18 @@
-class AuthServices {
+import { authRepositories } from "../reposetories/auth.reposetories";
+import { user } from "../types/types";
 
+class AuthServices {
+    async createUser(data: user) {
+        return await authRepositories.create(data)
+    }
+
+    async findByEmail(email: string) {
+        return await authRepositories.findByEmail(email)
+    }
+
+    async findByUserId(userId: number) {
+        return await authRepositories.findById(userId)
+    }
 }
 
 export const authServices = new AuthServices()
